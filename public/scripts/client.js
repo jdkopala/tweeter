e1b6c21bdd6d1f8a3f9ded15a4e28f0a3293244e
Jesse Kopala <jdkopala@gmail.com>
2022-06-22 17:49:27 -0600

    Implemented XSS protection with an escape function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,11 +4,17 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const escape = (str) => {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 const loadTweets = () => {
   $.get("/tweets")
     .then((data) => {
       renderTweets(data);
-    })
+    });
 };
 
 const renderTweets = (data) => {
@@ -20,7 +26,7 @@ const renderTweets = (data) => {
 
 const createTweetElement = (tweetData) => {
   let $tweet = (
-  `<article class="tweet">
+    `<article class="tweet">
     <header>
       <div>
         <img class="avatar" src=${tweetData.user.avatars}>
@@ -31,7 +37,7 @@ const createTweetElement = (tweetData) => {
       </span>
     </header>
     <div class="content">
-      ${tweetData.content.text}
+      ${escape(tweetData.content.text)}
     </div>
     <footer>
       <span>
@@ -43,7 +49,8 @@ const createTweetElement = (tweetData) => {
         <i class="fa-solid fa-heart-circle-plus"></i>
       </div>
     </footer>
-  </article>`)
+  </article>`);
+
   return $tweet;
 };
 
@@ -58,13 +65,13 @@ $(document).ready(() => {
       alert("Your tweet is too long");
       return false;
     } else if (!$tweetText) {
-      alert('Your tweet is too empty');
+      alert('Your tweet is empty');
       return false;
     } else if ($tweetText <= 140) {
       const $newTweet = $form.serialize();
       $.post("/tweets", $newTweet)
         .then(loadTweets());
-      $("#tweet-text").val("")
+      $("#tweet-text").val("");
     }
   });
 });
--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(() => {
-  let countStart = 140
+  let countStart = 140;
 
   $("#tweet-text").on('input', function() {
     let $counter = $(this).siblings("div").children("output");
     let $tweetLength = this.value.length;
-    let $charCount = countStart - $tweetLength
+    let $charCount = countStart - $tweetLength;
     $counter.text(`${$charCount}`);
 
     if ($charCount <= 0) {
       $counter.addClass("counter-negative");
-    } 
+    }
     if ($charCount >= 0) {
       $counter.removeClass("counter-negative");
     }
