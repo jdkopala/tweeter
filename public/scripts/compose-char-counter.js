796b92f0b92c24c6d1821d9765b654618d5f4952
Jesse Kopala <jdkopala@gmail.com>
2022-06-22 09:02:00 -0600

    changed variables to be in line with jQuery convention

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -2,16 +2,16 @@ $(document).ready(() => {
   let countStart = 140
 
   $("#tweet-text").on('input', function() {
-    let counter = $(this).siblings("div").children("output");
-    let tweetLength = this.value.length;
-    let charCount = countStart - tweetLength
-    counter.text(`${charCount}`);
+    let $counter = $(this).siblings("div").children("output");
+    let $tweetLength = this.value.length;
+    let $charCount = countStart - $tweetLength
+    $counter.text(`${$charCount}`);
 
-    if (charCount <= 0) {
-      counter.addClass("counter-negative");
+    if ($charCount <= 0) {
+      $counter.addClass("counter-negative");
     } 
-    if (charCount >= 0) {
-      counter.removeClass("counter-negative");
+    if ($charCount >= 0) {
+      $counter.removeClass("counter-negative");
     }
   });
 
