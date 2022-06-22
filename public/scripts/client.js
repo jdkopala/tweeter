20254a02c8024a8042d7606bff3f3bbaece05378
Jesse Kopala <jdkopala@gmail.com>
2022-06-22 14:36:58 -0600

    Implemented loadTweets function using ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,41 +4,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 $(document).ready(() => {
 
-  renderTweets = (data) => {
+  const loadTweets = () => {
+    $.get("/tweets")
+      .then((data) => {
+        renderTweets(data);
+      })
+  };
+  
+  const renderTweets = (data) => {
     for (let d of data) {
       let $tweet = createTweetElement(d);
       $("#tweets-container").append($tweet);
     }
   };
-
-  createTweetElement = (tweetData) => {
+  
+  const createTweetElement = (tweetData) => {
     let $tweet = (
     `<article class="tweet">
       <header>
@@ -67,13 +49,12 @@ $(document).ready(() => {
     return $tweet;
   };
 
-  renderTweets(data);
+  loadTweets();
 
   const $form = $(".new-tweet").children('form');
   $($form).on('submit',(event) => {
     event.preventDefault();
-    const $newTweet = $(this).serialize;
-    console.log($newTweet);
+    const $newTweet = $form.serialize();
     $.post("/tweets", $newTweet);
   })
 })
